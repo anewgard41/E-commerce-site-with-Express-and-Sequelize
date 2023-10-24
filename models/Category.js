52d1ff1383da77425d40b35ef9381d61e7fa3793
Anders Newgard <anne9707@colorado.edu>
2023-10-23 22:49:48 -0400

    wrote the logic for the Category.js file. Only two columns in this model are id and category_name. sequelize information at below the .init command was provided.

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Category only needs id and category_name columns as a part of its model. id column will be the primary key and will auto-increment. 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
