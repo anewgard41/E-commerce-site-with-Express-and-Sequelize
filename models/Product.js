cb6fd4f18a7f79d131d3694ffcdaf58790cbedc1
Anders Newgard <anne9707@colorado.edu>
2023-10-23 23:02:26 -0400

    finished writing the logic for the Product.js part of the model.

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,46 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      // Integer. Doesn't allow null values. Set as primary key. Uses auto increment.
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      // String. Doesn't allow null values.
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      // Decimal. Doesn't allow null values. Validates that the value is a decimal.
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      // Integer. Doesn't allow null values. Set a default value of 10. Validates that the value is numeric.
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      // Integer. References the Category model's id.
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
